559bb1d3208bb42a53867815513850161d1580da
vicbanta <vic@bantatechnologies.com>
2024-08-16 16:21:45 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://CommunityCommodityComputerCoaching.github.io',
+  site: 'https://wahkonsalodge.com',
   base: '/',
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
