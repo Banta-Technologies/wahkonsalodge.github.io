1757b4a26e9a2b45b5158f5282710fa5967631ef
vicbanta <vic@bantatechnologies.com>
2024-08-30 19:36:20 -0500

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -12,7 +12,7 @@ const blogCollection = defineCollection({
       alt: z.string(),
     }),
     publishDate: z.string().transform(str => new Date(str)),
-    author: z.string().default('c4giving'),
+    author: z.string().default('Wahkonsa Lodge'),
     category: z.string(),
     tags: z.array(z.string()),
   }),
