641c6731fb2f8286ddc9764137032247ba892a3c
Vic Banta <vic@bantatechnologies.com>
2026-06-21 10:49:17 -0500

    Sync Noni and Papa image gallery

--- src/data/noni-and-papa.ts
+++ src/data/noni-and-papa.ts
@@ -1,17 +1,72 @@
 export const noniAndPapaItems = [
   {
+    title: "Ducks Eat Shoes",
+    image: "/images/noni-and-papa/ducks-eat-shoes.png",
+    thumb: "/images/noni-and-papa/thumbs/ducks-eat-shoes.jpg",
+  },
+  {
+    title: "Bath Time",
+    image: "/images/noni-and-papa/bath-time.png",
+    thumb: "/images/noni-and-papa/thumbs/bath-time.jpg",
+  },
+  {
+    title: "Blackbird Summer",
+    image: "/images/noni-and-papa/blackbird-summer.png",
+    thumb: "/images/noni-and-papa/thumbs/blackbird-summer.jpg",
+  },
+  {
+    title: "Clean House",
+    image: "/images/noni-and-papa/clean-house.png",
+    thumb: "/images/noni-and-papa/thumbs/clean-house.jpg",
+  },
+  {
+    title: "Fear of Everyday",
+    image: "/images/noni-and-papa/fear-of-everyday.png",
+    thumb: "/images/noni-and-papa/thumbs/fear-of-everyday.jpg",
+  },
+  {
+    title: "Donkey Tooth",
+    image: "/images/noni-and-papa/donkey-tooth.png",
+    thumb: "/images/noni-and-papa/thumbs/donkey-tooth.jpg",
+  },
+  {
+    title: "Fiddle Farting",
+    image: "/images/noni-and-papa/fiddle-farting.png",
+    thumb: "/images/noni-and-papa/thumbs/fiddle-farting.jpg",
+  },
+  {
+    title: "Gravity",
+    image: "/images/noni-and-papa/gravity.png",
+    thumb: "/images/noni-and-papa/thumbs/gravity.jpg",
+  },
+  {
+    title: "Muffin Bottom",
+    image: "/images/noni-and-papa/muffin-bottom.png",
+    thumb: "/images/noni-and-papa/thumbs/muffin-bottom.jpg",
+  },
+  {
     title: "No Regrets",
     image: "/images/noni-and-papa/no-regrets.png",
-    thumb: "/images/noni-and-papa/thumbs/no-regrets.png",
+    thumb: "/images/noni-and-papa/thumbs/no-regrets.jpg",
   },
-  // Add images here after placing files in:
-  // - public/images/noni-and-papa/
-  // - public/images/noni-and-papa/thumbs/
-  //
-  // Example:
-  // {
-  //   title: "Fishing at the Lake",
-  //   image: "/images/noni-and-papa/fishing-at-the-lake.png",
-  //   thumb: "/images/noni-and-papa/thumbs/fishing-at-the-lake.png",
-  // },
+  {
+    title: "Noni and Her Furry Friends",
+    image: "/images/noni-and-papa/noni-and-her-furry-friends.png",
+    thumb: "/images/noni-and-papa/thumbs/noni-and-her-furry-friends.jpg",
+  },
+  {
+    title: "Papa Fishing Priorities",
+    image: "/images/noni-and-papa/papa-fishing-priorities.png",
+    thumb: "/images/noni-and-papa/thumbs/papa-fishing-priorities.jpg",
+  },
+  {
+    title: "Rock in Your Shoe",
+    image: "/images/noni-and-papa/rock-in-your-shoe.png",
+    thumb: "/images/noni-and-papa/thumbs/rock-in-your-shoe.jpg",
+  },
+  {
+    title: "Treat Her Well",
+    image: "/images/noni-and-papa/treat-her-well.png",
+    thumb: "/images/noni-and-papa/thumbs/treat-her-well.jpg",
+  },
 ] as const;
